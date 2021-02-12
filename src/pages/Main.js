aa8ec177063ecc0ca812a9b44725ac059e4d0e85
Alex <64551315+Alex-Ferreira-Santos@users.noreply.github.com>
2021-02-12 11:33:48 -0300

    carregando os dados de pouco em pouco

--- src/pages/Main.js
+++ src/pages/Main.js
@@ -3,33 +3,37 @@ import {View,Text,FlatList,SafeAreaView,ScrollView} from 'react-native'
 import Pokemon from '../components/Pokemon'
 import styles from '../styles/main'
 
+let inicial = 0
+
 class Main extends Component{ 
   constructor(props){
     super(props)
     this.state = {
-      pokemons:[]
+      pokemons:[],
     }
     this.carregaPokemons()
     this.setPokemons = this.setPokemons.bind(this)
+    this.carregaPokemons = this.carregaPokemons.bind(this)
   }
   
-  carregaPokemons(){
-    fetch('https://pokeapi.co/api/v2/pokemon?offset=0&limit=20')
+  carregaPokemons(){ 
+    fetch(`https://pokeapi.co/api/v2/pokemon?offset=${inicial}&limit=10`)
     .then(response => response.json())
     .then(data => {
       this.setPokemons(data.results)
     })
+    inicial = inicial + 10
   }
 
   setPokemons(data){
-    this.setState({pokemons: data})
+    this.setState({pokemons: [...this.state.pokemons,...data]})
   }
 
   PokemonShow(pokemon) {
     const pokemons = pokemon.item
     const pokemonNumber = pokemons.url.replace('https://pokeapi.co/api/v2/pokemon/','').replace('/','');
     return (
-      <View key={pokemonNumber}>
+      <View key={pokemon.index} style={{alignItems:'center'}}>
           <Pokemon name={pokemons.name} id={pokemonNumber}/>
       </View>
     )
@@ -51,6 +55,8 @@ class Main extends Component{
             keyExtractor={(pokemon) => pokemon.name}
             contentContainerStyle={styles.contentContainerStyle}
             renderItem={this.PokemonShow}
+            onEndReached={this.carregaPokemons}
+            onEndReachedThreshold={0.2}
             />          
         </View>
     )
